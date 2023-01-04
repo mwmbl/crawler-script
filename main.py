94e5891411a76da5ffb64f244de7902c08a27053
Daoud Clarke <daoud.clarke@gmail.com>
2023-01-04 22:13:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+from justext import core, utils
+
+
+def fetch(url):
+    response = requests.get(url)
+    paragraphs = core.justext(response.content, utils.get_stoplist("English"))
+    for paragraph in paragraphs:
+        print("Paragraph", paragraph.text)
+
+
+if __name__ == '__main__':
+    fetch("https://blog.mwmbl.org/articles/fall-2022-update/")
